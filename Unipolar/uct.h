48ccf7b0cedaf78a031dd80fdd1290570fa0813e
EricXingSJTU <664734926@qq.com>
2015-12-11 20:44:55 +0800

    uct_v1

--- Unipolar/uct.h
+++ Unipolar/uct.h
@@ -2,106 +2,100 @@
 #define UNIPOLAR_UCT_H_
 #include "board.h"
 #include "def.h"
+#include "mc.h"
 #include <assert.h>
 #include <stdlib.h>
 #include <math.h>
+#include <set>
 using namespace unipolar;
 
 template <int BOARD_SIZE>
 class UCT {
 private:
-	struct Node {
-		PositionIndex pos;
-		Count num;
-		Value val, MCval;
-		Result res;
-		Node *pa, *son, *bro;
-		Node(PositionIndex po = -1, Node *pa = nullptr, Node *so = nullptr, Node *br = nullptr) : pos(po), num(0), val(0), MCval(0), pa(pa), son(so), bro(br) {}
-	};
+    struct Node {
+        PositionIndex pos;
+        Count num;
+        Val val;
+        Node *pa, *son, *bro;
+        Node(PositionIndex po = -1, Node *pa = nullptr, Node *so = nullptr, Node *br = nullptr) : pos(po), num(0), val(0), pa(pa), son(so), bro(br) {}
+    };
 public:
-	UCT() = default;
-	~UCT() = default;
-	void AddNode(Node *newnode);
-	void DelNode(Node *oldnode);
-	Count GetNum(Node *node) const;
-	Value GetVal(Node *node) const;
-	Value GetUCB(Node *node);
-	Result GetRes(Node *node) const;
-	Result GenMC(const Board &board, PointState state);
-	void Recompute(Node *pa);
-	void UpdatePa(Node *pa, Value val);
-	//Move GenMove(const Board &board, PointState state);
+    UCT() = default;
+    ~UCT() = default;
+    static void GenChild(Node *node, Board &board, PointState state);
+    static float UCB(Node *node);
+    static void FindBestChild(Node *node);
+    static int MCSimulation(Board &board, Node *node, PointState state);
+    static Move GenMove(const Board &board, PointState state);
 
 private:
-	Node root;
+    Node root;
 };
 
-void UCT::AddNode(Node *newnode) {
-    newnode->bro = this->son;
-    this->son = newnode;
-    newnode->pa = this;
-}
-
-void UCT::DelNode(Node *oldnode) {
-    Node *tmp = oldnode;
-    if(oldnode->pa->son->pos == oldnode->pos) {
-        this->son = oldnode->bro;
-        delete oldnode;
-    }
-    else {
-        tmp = this->son;
-        while(tmp->bro != NULL && tmp->bro->pos != oldnode->pos)
-            tmp = tmp->bro;
-        tmp->bro = oldnode->bro;
-        delete oldnode;
+template <int BOARD_SIZE>
+void UCT<BOARD_SIZE>::GenChild(Node *node, Board &board, PointState state) {
+    Board actBoard(board);
+    std::set<PositionIndex> playable;
+    playable = actBoard.GetPlayablePosition(state);
+    std::set<PositionIndex>::iterator it;
+    for(it = playable.begin(); it != playable.end(); it++) {
+        Node *newchild = new Node(*it, nullptr, nullptr, nullptr);
+        newchild.bro = node.son;
+        node.son = newchild;
     }
 }
 
-Count UCT::GetNum(Node *node) const {
-    return node.num;
+template <int BOARD_SIZE>
+float UCT<BOARD_SIZE>::UCB(Node *node) {
+    return node.val + sqrt(2 * logf(node.pa.num - 1) / node.num);
 }
 
-Value UCT::GetVal(Node *node) const {
-    return node.val;
-}
-
-Value UCT::GetUCB(Node *node) {
-    assert(node.pa);
-    return node.val / node.num + sqrt(2 * logf(node.pa.num) / node.num);
-}
-
-Result UCT::GetRes(Node *node) const {
-    return node.res;
-}
-
-PointState GenMC(const Board &board, PointState state) {
-
-}
-
-void UCT::Recompute(Node *pa) {
-    Node *tmp;
-    pa.num = 1;
-    pa.val = MCval;
-    tmp = pa.son;
-    while(tmp != NULL) {
-        if(tmp.res != LOOSER) {
-            pa.num += tmp.num;
-            pa.val += tmp.val;
+template <int BOARD_SIZE>
+void UCT<BOARD_SIZE>::FindBestChild(Node *node) {
+    Node *act = node.son;
+    Node *prebr;
+    float maxval = UCB(act);
+    while(act.bro != NULL) {
+        prebr = act;
+        act = act.bro;
+        if(UCB(act) > maxval) {
+            prebr.bro = act.bro;
+            act.bro = node.son;
+            node.son = act;
         }
-        tmp = tmp.bro;
-    }
-    if(pa.pa)
-        Recompute(pa.pa);
-}
-
-void UCT::UpdatePa(Node *pa, Value val) {
-    if(node.pa) {
-        node.pa.num += 1;
-        node.pa.val += (1 - val);
-        node.pa.UpdatePa(1 - value);
     }
 }
 
-//Move GenMove(const Board &board, PointState state) {}
+template <int BOARD_SIZE>
+int UCT<BOARD_SIZE>::MCSimulation(Board &board, Node *node, PointState state) {
+    Board actBoard(board);
+    Move move;
+    FindBestChild(node);
+    Node *act = node.son;
+    move.state = state;
+    move.position = act.pos;
+    actBoard.PlayMove(move);
+    act.num += 1;
+    if(act.son == NULL) {
+        if(act.num > 1) {
+            GenChild(act, actBoard, 1 - state);
+            act.val = 1 - MCSimulation(actBoard, act, 1 - state);
+        }
+        act.val = MC.Simulate(actBoard, 1 - state);
+        return act.val;
+    }
+    else
+        act.val = 1 - MCSimulation(actBoard, act, 1 - state);
+}
 
-#endif
+template <int BOARD_SIZE>
+Move UCT<BOARD_SIZE>::GenMove(const Board &board, PointState state) {
+    Board uctBoard(board);
+    root.num = 1;
+    GenChild(root, uctBoard, state);
+    while(root.num != 100) {
+        MCSimulation(root);
+    }
+    FindBestChild(root);
+    return Move(1 - state, root->son.pos);
+}
